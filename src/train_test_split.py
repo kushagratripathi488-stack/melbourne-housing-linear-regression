267cd08bf302248fefc886b8236c48e27d0d4689
unknown <kushagratripathi488@gmail.com>
2026-06-24 11:47:45 +0530

    Implemented the train test and split features in src

---
+++ src/train_test_split.py
@@ -0,0 +1,27 @@
+import copy 
+import random
+def train_test_data(data,test_size) :
+    data_copy=copy.deepcopy(data)
+    random.shuffle(data_copy)
+    split_idx = int(len(data_copy)*(1-test_size))
+    train_data=data_copy[:split_idx]
+    test_data=data_copy[split_idx:]
+    return train_data,test_data
+
+def split_feature_target(train_data,test_data,target_idx) : 
+    X_train=[]
+    X_test=[]
+    y_train=[]
+    y_test=[]
+    for row in train_data: 
+        X_train.append(train_data[:target_idx]+train_data[target_idx+1:])
+        y_train.append(train_data[target_idx])
+    for row in train_data: 
+        X_test.append(test_data[:target_idx]+test_data[target_idx+1:])
+        y_test.append(test_data[target_idx])
+    return X_train,y_train,X_test,y_test
+ 
+def train_test_split(data,target_idx,test_size):
+    train_data,test_data=train_test_data(data,test_size)
+    X_train,y_train,X_test,y_test=split_feature_target(train_data,test_data,target_idx)
+    return X_train,y_train,X_test,y_test
